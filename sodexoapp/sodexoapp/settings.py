6c08004c68a8f27ff60caa1a81dd5d1686d0bf28
leandromaia <leandroc@inatel.br>
2014-06-05 13:47:37 -0300

    Configuraçao static files em producao

--- sodexoapp/sodexoapp/settings.py
+++ sodexoapp/sodexoapp/settings.py
@@ -2,6 +2,7 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
@@ -67,7 +68,7 @@ SODEXOCLIENT_CREATED_MESSAGE = 'Seu cadastro para acesso ao Sodexoapp ' \
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -105,7 +106,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- sodexoapp/sodexoapp/urls.py
+++ sodexoapp/sodexoapp/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,3 +11,9 @@ urlpatterns = patterns('',
     url(r'^$', 'access.views.show_system'),
     url(r'^consultation/', include('consultation.urls'))
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$',\
+        'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
